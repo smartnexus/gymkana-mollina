4faec5b8c0a8b6675d4bbd769e9020b54b22c2dd
smartnexus <angrodboh@alum.us.es>
2023-08-18 12:30:41 +0200

    Project base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './fonts/AlbertSans-Bold.ttf'
+import './fonts/AlbertSans-Regular.ttf'
+import './styles/index.css'
+
+import App from './app/App';
+import { LoadingSpinner } from './app/Loading';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+	// <React.StrictMode>
+	<Suspense fallback={<LoadingSpinner/>}>
+		<App/>
+	</Suspense>
+	// </React.StrictMode>
+);
