bf579ae8d357ce479054dcf394b02f758a586a73
smartnexus <angrodboh@alum.us.es>
2023-08-18 14:32:45 +0200

    Hide secrets

---
+++ src/config/firebase.example.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+	apiKey: "API_KEY",
+	authDomain: "AUTH_DOMAIN",
+	projectId: "PROJECT_ID",
+	storageBucket: "STORAGE_BUCKET",
+	messagingSenderId: "MESSAGING_SENDER_ID",
+	appId: "APP_ID",
+	databaseURL: "DATABASE_URL"
+};
