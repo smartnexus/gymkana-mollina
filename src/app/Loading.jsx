96a9cfdce13949d16b2ca72b35c8fd7d486964ce
smartnexus <angrodboh@alum.us.es>
2023-08-25 17:53:09 +0200

    Considering slow connections due to 3g

--- src/app/Loading.jsx
+++ src/app/Loading.jsx
@@ -1,3 +1,6 @@
 export const LoadingSpinner = () => (
-    <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <>
+        <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <p className="lds-text">Esto esta tardando un poco más de lo habitual, un momento por favor...</p>
+    </>
 )
